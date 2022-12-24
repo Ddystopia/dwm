2a57ca67f1c8182e7b515c19ecf976dc7591caf3
Ddystopia <sashkamaman@gmail.com>
2022-12-24 22:22:39 +0100

    upd

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,13 @@
-austart "sxhkd -c $XDG_CONFIG_HOME/sxhkd/sxhkdrc-dwm" sxhkd
+# austart "sxhkd -c $XDG_CONFIG_HOME/sxhkd/sxhkdrc-dwm" sxhkd
+austart sxhkd
 austart picom
-austart "xfce4-power-manager 2> /dev/null" xfce4-power-manager
+# austart "xfce4-power-manager 2> /dev/null" xfce4-power-manager
 austart copyq
 austart keepassxc
 austart barstatus
-austart "monerod --detatch" monerod
+# austart "monerod --detach" monerod
+synclient TouchpadOff=1
+log_battery_cycles
 
 bluetoothctl power on
 
