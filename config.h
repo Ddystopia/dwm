ddd1462005977b2f3e9d73a7e63c63426b273329
Ddystopia <alexanderbabak@proton.me>
2023-04-24 20:55:08 +0200

    Checkpoint

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	/* { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, */
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY|ALTKEY,                XK_j,      focusmon,       {.i = -1 } },
